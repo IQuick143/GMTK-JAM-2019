b34fd8755f942f37dfd3b22d7fecf015f88bdb89
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2019-08-04 00:06:36 +0200

    adding transitions, testing new controller and organizing assets

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+    public Animator animator;
+
+    public void FadeNextLevel() {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/WadorenTest/WadorenTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WadorenTest : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private KeyCode key = KeyCode.A;
+    private float direction = 1f; //-1 or 1
+    private float speed = 5f;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(key) || Input.GetKey(key))
+        {
+            rb.velocity = new Vector2(direction * speed, 0f);
+        }
+        if (Input.GetKeyUp(key))
+        {
+            //Character turns around
+            direction = -direction;
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
