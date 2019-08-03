345d337f8cbf7342045ccdb7388f230ab73845d0
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2019-08-03 16:30:01 +0200

    player attaches onto platforms

--- Assets/Scripts/Actuators/Elevator.cs
+++ Assets/Scripts/Actuators/Elevator.cs
@@ -9,22 +9,22 @@ public class Elevator : Actuator {
 
     private bool isActive;
     private int currPointTarget = 0;
-    private Rigidbody2D rb;
+    private Vector3 velocity;
 
-    private void Start() {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate() {
+    private void Update() {
         if (isActive) {
             //If this cycle it will surpass the position of the target point, place it on the target point
-            if (Vector2.Distance(transform.position, points[currPointTarget].position) < rb.velocity.magnitude * Time.fixedDeltaTime) {
+            if (Vector2.Distance(transform.position, points[currPointTarget].position) < velocity.magnitude * Time.deltaTime) {
                 transform.position = points[currPointTarget].position;
                 currPointTarget++;
                 if (currPointTarget == points.Length) {
                     currPointTarget = 0;
                 }
-                rb.velocity = GetVelocityToTarget();
+                velocity = GetVelocityToTarget();
+            }
+            else
+            {
+                transform.Translate(velocity * Time.deltaTime);
             }
         }
 
@@ -32,17 +32,16 @@ public class Elevator : Actuator {
 
     public override void OnActivate() {
         isActive = true;
-        rb.velocity = GetVelocityToTarget();
+        velocity = GetVelocityToTarget();
     }
 
     public override void OnDeactivate() {
         isActive = false;
-        rb.velocity = Vector2.zero;
+        velocity = Vector2.zero;
     }
 
-    private Vector2 GetVelocityToTarget()
-    {
-        Vector2 dir = points[currPointTarget].position - transform.position;
+    private Vector3 GetVelocityToTarget() {
+        Vector3 dir = points[currPointTarget].position - transform.position;
         dir.Normalize();
         dir *= platformSpeed;
         return dir;
