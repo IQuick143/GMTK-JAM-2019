88792c2ccce3b21d92bbe833de5caa7b9c85bd6b
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2019-08-03 22:40:18 +0200

    modifying look of level 001

---
+++ Assets/Scripts/FrameCreator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameCreator : MonoBehaviour {
+
+    public GameObject frame;
+    public int instances;
+    public float startingDist;
+    public float distanceIncrease;
+    public float distanceExp;
+
+    void Start() {
+        for (int i = 0; i < instances; ++i)
+        {
+            Instantiate(frame, new Vector3(0f, 0f, distanceIncrease * i + Mathf.Pow(distanceExp, i)), Quaternion.identity);
+        }
+    }
+
+    void Update() {
+        
+    }
+}
