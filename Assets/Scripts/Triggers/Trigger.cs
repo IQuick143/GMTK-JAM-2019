2ee5336f3c546c740736abd1e472c83e189db915
IQuick 143 <IQuick143cz@gmail.com>
2019-08-03 09:19:48 +0200

    Created base scripts for actuators and triggers

---
+++ Assets/Scripts/Triggers/Trigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Trigger : MonoBehaviour {
+	protected List<Actuator> triggeredActuators;
+	private bool _active = false;
+	public bool active {
+		protected set {
+			if (_active != value) {
+				_active = value;
+				UpdateGizmos();
+				UpdateWire();
+			}
+			
+		}
+		get {
+			return _active;
+		}
+	}
+
+	void UpdateGizmos() {
+		this.triggeredActuators.ForEach(
+			(a) => {
+				if (this._active) {
+					a.OnActivate();
+				} else {
+					a.OnDeactivate();
+				}
+			}
+		);
+	}
+
+	void UpdateWire() {
+		//TODO: WIRE MECHANICS
+	}
+
+	public void AddActuator(Actuator a) {
+		this.triggeredActuators.Add(a);
+	}
+
+	//void OnCollisionEnter2D();
+}
