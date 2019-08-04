c24c9f0565ff523be4b51c85375271ed52a79f70
IQuick 143 <IQuick143cz@gmail.com>
2019-08-04 14:07:13 +0200

    GOD DOOR COMMIT 1
    Not complete, not broken what a time to commit

--- Assets/Scripts/Actuators/VerticalForceField.cs
+++ Assets/Scripts/Actuators/VerticalForceField.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class VerticalForceField : MonoBehaviour {
 
-    public float ySpeed;
-    
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        Rigidbody2D colRb = collision.attachedRigidbody;
-        if(colRb != null)
-        {
-            //TODO: Add an acceleration so it feels better
-            colRb.velocity = new Vector3(colRb.velocity.x, ySpeed);
-        }
-    }
+	public float ySpeed;
+	public float Force = 1f;
+	
+	private void OnTriggerStay2D(Collider2D collision) {
+		Rigidbody2D colRb = collision.attachedRigidbody;
+		if (colRb != null) {
+			//TODO: Add an acceleration so it feels better
+			if (colRb.velocity.y < ySpeed) colRb.AddForce(-Physics2D.gravity + Force * Vector2.up);
+			//colRb.velocity = new Vector3(colRb.velocity.x, ySpeed);
+		}
+	}
 }
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,16 +6,19 @@ using UnityEngine;
 //https://www.youtube.com/watch?v=MFQhpwc6cKE
 //Comments say lerp is not good for cameras, use SmoothDamp instead
 
-public class CameraFollow : MonoBehaviour
-{
+public class CameraFollow : MonoBehaviour {
 
     public Transform target;
-    public Vector3 offset;
+    public Vector3 offset = Vector3.zero;
     public float distanceDamp;
 
     private Vector3 velocity = Vector3.one;
-    private Vector3 desiredPosition;
-    private Vector3 smoothedPosition;
+
+	private void Start() {
+		if (offset == Vector3.zero) {
+			offset = this.transform.position - this.target.position;
+		}
+	}
 
     private void LateUpdate() {
         transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, distanceDamp);
